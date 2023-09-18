315973bf7d6b7e6345b49d47c80c44c445cc4c1b
rachanni <111679517+rachanni@users.noreply.github.com>
2023-09-19 00:44:45 +0530

    beginning of commit

---
+++ soap-ws/src/main/java/com/baeldung/springsoap/SoapWsApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.springsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWsApplication.class, args);
+	}
+
+}
---
+++ soap-ws/src/test/java/com/baeldung/springsoap/SoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.springsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
